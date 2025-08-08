cdfbcb784c761e8bb36689079c1b872cf1bed1de
AbidShazid3 <abidshazid3@gmail.com>
2025-08-08 19:19:18 +0600

    part 1 completed

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -2,8 +2,14 @@
 
 const About = () => {
     return (
-        <div>
-            <h1>this is about page</h1>
+        <div className="text-center px-4 py-16">
+            <h1 className="text-3xl pb-5 font-bold">This is about page</h1>
+            <p>Welcome to TourVisor, your trusted travel partner for unforgettable journeys.
+                We believe travel is more than just visiting places — it’s about creating memories, exploring cultures, and experiencing the world in a whole new way.
+
+                From breathtaking natural wonders to vibrant cityscapes, we offer carefully curated tours and travel packages that suit every taste and budget. Our mission is to make your trip simple, enjoyable, and stress-free — so you can focus on making memories that last a lifetime.
+
+                With expert guidance, secure bookings, and 24/7 support, we`re here to turn your dream vacations into reality.</p>
         </div>
     );
 };
