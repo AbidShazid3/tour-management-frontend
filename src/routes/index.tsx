405c5e9fe6688152a0ca77b20196d7835e7d2944
AbidShazid3 <abidshazid3@gmail.com>
2025-08-19 07:13:42 +0600

    added admin and user sidebar items

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,11 @@ import DashboardLayout from "@/components/layout/DashboardLayout";
 import About from "@/pages/About/About";
 import Login from "@/pages/Login/Login";
 import Register from "@/pages/Register/Register";
-import Bookings from "@/pages/User/Bookings";
 import Verify from "@/pages/Verify/Verify";
 import { generateRoutes } from "@/utils/generateRoutes";
 import { createBrowserRouter } from "react-router";
 import { adminSidebarItems } from "./adminSidebarItems";
+import { userSidebarItems } from "./userSidebarItems";
 
 
 export const router = createBrowserRouter([
@@ -29,12 +29,7 @@ export const router = createBrowserRouter([
   {
     path: "/user",
     Component: DashboardLayout,
-    children: [
-      {
-        path: "bookings",
-        Component: Bookings,
-      },
-    ]
+    children: [...generateRoutes(userSidebarItems)],
   },
   {
     path: "/login",
